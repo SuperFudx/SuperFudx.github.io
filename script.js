54cbd913a68330d86801139234c1e4e356d80994
骑着蜗牛闯红灯 <88933438+SuperFudx@users.noreply.github.com>
2024-03-13 10:36:43 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,13 @@
-/*function calculateSum() {
-    // 获取当前时间
-    const currentDate = new Date();
-    // 获取月份、日期和星期几
-    const month = (currentDate.getMonth() + 1).toString();
-    const date = （currentDate.getDate() + 1).toString();
-    const dayOfWeek = currentDate.getDay(); // 0 是星期日，1 是星期一，以此类推
-
-    // 计算月份、日期和星期几的和
-    let sum1 = 0;
-    let sum2 = 0;
-    for (let i = 0; i < month.length; i++) {
-        sum1 += parseInt(month[i]);
-    }
-    for (let i = 0; i < date.length; i++) {
-        sum2 += parseInt(date[i]);
-    }
-    sum1 += dayOfWeek;
-    sum2 += dayOfWeek；
-
-    // 返回结果
-    return sum1;//+ sum2.toString().padStart(2, '0');
-}*/
+function calculateSum() {
+  const now = new Date();
+  const sum = now.getHours() + now.getMinutes() + now.getSeconds();
+  return sum;
+}
 
 function displaySum() {
   const sumElement = document.getElementById('sum');
-  //const sum = calculateSum();
-  sumElement.textContent = `当前注册码12:`;
+  const sum = calculateSum();
+  sumElement.textContent = `Sum of current time: ${sum}`;
 }
 
 // Update sum every second
