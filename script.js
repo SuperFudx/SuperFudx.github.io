aa4121cab3aa2966bb1c887de670a740c335bcc6
骑着蜗牛闯红灯 <88933438+SuperFudx@users.noreply.github.com>
2024-03-13 10:54:28 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,21 +4,24 @@ function calculateSum() {
 
     // 获取月份、日期和星期几
     const month = (currentDate.getMonth() + 1).toString();
-    //const date = currentDate.getDate().toString();
+    const date = currentDate.getDate().toString();
     const dayOfWeek = currentDate.getDay(); // 0 是星期日，1 是星期一，以此类推
 
     // 计算月份、日期和星期几的和
     let sum = 0;
+	let sum1 = 0;
     for (let i = 0; i < month.length; i++) {
         sum += parseInt(month[i]);
     }
-    //for (let i = 0; i < date.length; i++) {
-    //    sum += parseInt(date[i]);
-    //}
-    sum += dayOfWeek;
+	
+	 for (let i = 0; i < date.length; i++) {
+        sum1 += parseInt(date[i]);
+    }
+   sum += dayOfWeek;
+   sum1 += dayOfWeek;
 
     // 返回结果
-    return sum.toString().padStart(2, '0');
+    return sum.toString().padStart(2, '0') + sum1.toString().padStart(2, '0');
 }
 
 function displaySum() {
