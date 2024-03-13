5348ff41236e0d7b4b3af522bf2c738807f6a783
骑着蜗牛闯红灯 <88933438+SuperFudx@users.noreply.github.com>
2024-03-13 12:45:29 +0800

    Update script.js

--- script.js
+++ script.js
@@ -25,14 +25,40 @@ function calculateSum() {
     return sum1.toString().padStart(2, '0') + sum2.toString().padStart(2, '0');
 }
 
+function calc() {
+    // 获取当前时间
+    const currentDate = new Date();
+
+    // 获取月份、日期和星期几
+    const month1 = (currentDate.getMonth() + 1) % 10;
+    const month2 = Math.floor(currentDate.getMonth() / 10);
+    const date1 = currentDate.getDate() % 10;
+    const date2 = currentDate.getDate() / 10;
+    const hour1 = currentDate.getHours() % 10;
+    const hour2 = Math.floor(currentDate.getHours() / 10);
+
+    const dayOfWeek = currentDate.getDay(); // 0 是星期日，1 是星期一，以此类推
+    //计算
+    var firstNumber = month1 - month2;
+    var secondNumber = date1 - date2;
+    var thirdNumber = hour1 - hour2;
+
+    var fifthNumber = firstNumber + secondNumber + thirdNumber;
+    var newPart1 = fifthNumber * dayOfWeek;
+    var newPart2 = fifthNumber + dayOfWeek;
+
+    // 返回结果
+    return newPart1.toString().padStart(2, '0') + newPart2.toString().padStart(2, '0');
+}
+
 function displaySum() {
   const sumElement = document.getElementById('registeCode');
   const sum = calculateSum();
   sumElement.textContent = `注册码信息: Ctrl+PY+${sum}`;
 	
   const sumElement2 = document.getElementById('backCode');
-  //const sum = calculateSum();
-  sumElement2.textContent = `后台码信息: 暂未实现`;
+  const sum2 = calc();
+  sumElement2.textContent = `后台码信息: Ctrl+FPI+${sum2}`;
 }
 
 // Update sum every second
